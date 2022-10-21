7563b1d2552b10a70a36a1a7b302df0d01259fc1
sanyasi1310 <swapn1310@gmail.com>
2022-10-21 14:07:28 -0700

    grid view

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Utils/routes.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -15,10 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          primaryTextTheme: GoogleFonts.latoTextTheme()),
+      theme: MyTheme.lightTheme(context),
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,95 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_catalog/widgets/drawer.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productsData).map((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MyBAPS App"),
+        title: const Text("MyBAPS App", style: TextStyle(color: Colors.black)),
       ),
-      body: Container(
-          child: const Center(
-        child: Text('Jay Swaminarayan'),
-      )),
-      drawer: Mydrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ?
+            // ? ListView.builder(
+            //     itemBuilder: (context, index) {
+            //       return ItemWidget(
+            //         item: CatalogModel.items[index],
+            //       );
+            //     },
+            //     itemCount: CatalogModel.items.length,
+            //   )
+            GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          decoration:
+                              const BoxDecoration(color: Colors.deepPurple),
+                          padding: const EdgeInsets.all(12),
+                          child: Text(
+                            item.name,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                          fit: BoxFit.cover,
+                        ),
+                        footer: Container(
+                            decoration:
+                                const BoxDecoration(color: Colors.black),
+                            padding: const EdgeInsets.all(12),
+                            child: Text(
+                              item.price.toString(),
+                              style: const TextStyle(color: Colors.white),
+                            )),
+                      ));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+      drawer: const Mydrawer(),
     );
   }
 }
